cb193c0c1ac6489097c5d5fb970e164bf0fdf473
Guillaume P <gcconantc@gmail.com>
2022-01-08 19:18:19 +0100

    Working validation layer

--- src/Rebulkan.cpp
+++ src/Rebulkan.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv)
 	glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
 	glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
-	const std::vector<const char*> validationLayers = { "VK_LAYER_LUNARG_standard_validation" };
+	const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
 	const std::vector<const char*> deviceExtensions = { VK_KHR_SWAPCHAIN_EXTENSION_NAME };
 
 	VkApplicationInfo appInfo {};
@@ -24,18 +24,27 @@ int main(int argc, char** argv)
 	appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	appInfo.pApplicationName = "Rebulkan";
 	appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
-	
+
+	uint32_t glfwExtensionCount = 0;
+	const char** glfwExtensions;
+	glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
 	VkInstanceCreateInfo createInfo {};
 	createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
 	createInfo.pApplicationInfo = &appInfo;
-	//createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
-	//createInfo.ppEnabledLayerNames = validationLayers.data();
-
+	createInfo.enabledExtensionCount = glfwExtensionCount;
+	createInfo.ppEnabledExtensionNames = glfwExtensions;
+	createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+	createInfo.ppEnabledLayerNames = validationLayers.data();
 
 	VkInstance instance = VK_NULL_HANDLE;
-	if (VK_SUCCESS != vkCreateInstance(&createInfo, nullptr, &instance))
+	VkResult result = VK_SUCCESS;
+
+	result = vkCreateInstance(&createInfo, nullptr, &instance);
+
+	if (VK_SUCCESS != result)
 	{
-		Logger->critical("Can't create VK instance");
+		Logger->critical("Can't create VK instance : {}", result);
 		return -1;
 	}
 
