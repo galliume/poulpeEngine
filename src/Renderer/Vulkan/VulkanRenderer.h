6869d3e84d9df421ad506aef886bb94bb31618d5
Guillaume P <gcconantc@gmail.com>
2022-01-08 22:07:43 +0100

    Check available extensions

--- src/Renderer/Vulkan/VulkanRenderer.h
+++ src/Renderer/Vulkan/VulkanRenderer.h
@@ -4,10 +4,15 @@ namespace Rebulk {
 
 	class VulkanRenderer
 	{
-		public:
-			VulkanRenderer(uint32_t extensionCount, const char* const* extensions);
-			~VulkanRenderer();
-		private:
-			VkInstance m_Instance = VK_NULL_HANDLE;
+	public:
+		VulkanRenderer(uint32_t extensionCount, const char* const* extensions);
+		~VulkanRenderer();
+
+	private:
+		void CreateInstance(uint32_t extensionCount, const char* const* extensions);
+		void EnumerateExtensions();
+
+	private:
+		VkInstance m_Instance = VK_NULL_HANDLE;
 	};
 }
