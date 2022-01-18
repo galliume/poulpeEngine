e8c07181846233bf0e5184a9cca647687f8cb178
Guillaume P <gcconantc@gmail.com>
2022-01-18 23:50:36 +0100

    fix for windows

--- vendor/GLFW/premake5.lua
+++ vendor/GLFW/premake5.lua
@@ -13,18 +13,27 @@ project "GLFW"
     }
     
 	filter "system:windows"
-        buildoptions { "-std=c11", "-lgdi32" }
+        buildoptions { "-std=c14", "-lgdi32" }
         systemversion "latest"
         staticruntime "on"
         
         files
         {
+            "src/context.c",
+            "src/init.c",
+            "src/input.c",
+            "src/monitor.c",
+            "src/vulkan.c",
+            "src/window.c",
             "src/win32_init.c",
             "src/win32_joystick.c",
             "src/win32_monitor.c",
             "src/win32_time.c",
             "src/win32_thread.c",
-            "src/win32_window.c"
+            "src/win32_window.c",
+            "src/wgl_context.c",
+            "src/egl_context.c",
+            "src/osmesa_context.c"
         }
 
 		defines 
