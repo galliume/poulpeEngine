db59e3d6c28b374f14d801d1ffe379d6e88c1538
Guillaume P <gcconantc@gmail.com>
2022-01-19 18:33:59 +0100

    create image views

--- src/Renderer/Vulkan/VulkanRenderer.cpp
+++ src/Renderer/Vulkan/VulkanRenderer.cpp
@@ -528,6 +528,10 @@ namespace Rebulk {
 
 	VulkanRenderer::~VulkanRenderer()
 	{
+		for (auto imageView : m_SwapChainImageViews) {
+			vkDestroyImageView(m_Device, imageView, nullptr);
+		}
+
 		vkDestroySwapchainKHR(m_Device, m_SwapChain, nullptr);
 		vkDestroyDevice(m_Device, nullptr);
 
@@ -540,6 +544,40 @@ namespace Rebulk {
 
 		m_Messages.emplace_back("VK instance destroyed");
 		Notify();
+	}
+
+	void VulkanRenderer::CreateImageViews()
+	{
+		m_SwapChainImageViews.resize(m_SwapChainImages.size());
+
+		for (size_t i = 0; i < m_SwapChainImages.size(); i++) {
+			VkImageViewCreateInfo createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = m_SwapChainImages[i];
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = m_SwapChainImageFormat;
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+
+			VkResult result;
+
+			result = vkCreateImageView(m_Device, &createInfo, nullptr, &m_SwapChainImageViews[i]);
+			
+			if (result != VK_SUCCESS) {
+				m_Messages.emplace_back("failed to create image views!");
+			} else {
+				m_Messages.emplace_back("created image views sucessfully");
+			}
+
+			Notify();
+		}
 	}
 
 	void VulkanRenderer::Attach(IObserver* observer)
