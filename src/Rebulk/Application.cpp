af5cc579ffdb072f7f7b3abf7c6b1c9bb04402d2
Guillaume P <gcconantc@gmail.com>
2022-02-04 22:53:09 +0100

    move readFile to Tools
    Former-commit-id: 77f320ef01b4b1806c6ee4b81e6b8de90204f850 [formerly a659b454f697952493110b462312da408c4a40d5] [formerly 3e6370eef82e7572c43fcb7e79dbc6eba4e0809b [formerly 3e6370eef82e7572c43fcb7e79dbc6eba4e0809b [formerly 077a8a6d706fde4ec72e48dccce95fc50ad18b3a]]]
    Former-commit-id: 3e6370eef82e7572c43fcb7e79dbc6eba4e0809b
    Former-commit-id: a3e0cdd6895d4d3f84dc89201b5859137ab87e17
    Former-commit-id: 23e10dcbbbe952a61b3ee9e427c0388985129294

--- src/Rebulk/Application.cpp
+++ src/Rebulk/Application.cpp
@@ -7,25 +7,6 @@ namespace Rbk
 		auto renderer = reinterpret_cast<Rbk::VulkanRenderer*>(glfwGetWindowUserPointer(window));
 		renderer->m_FramebufferResized = true;
 	}
-
-	static std::vector<char> ReadFile(const std::string& filename)
-	{
-		std::ifstream file(filename, std::ios::ate | std::ios::binary);
-
-		if (!file.is_open()) {
-			throw std::runtime_error("failed to open file!");
-		}
-
-		size_t fileSize = (size_t)file.tellg();
-		std::vector<char> buffer(fileSize);
-
-		file.seekg(0);
-		file.read(buffer.data(), fileSize);
-
-		file.close();
-
-		return buffer;
-	}
 	
 	Application* Application::s_Instance = nullptr;
 
---
+++ src/Rebulk/Core/Tools.h
@@ -0,0 +1,24 @@
+#pragma once
+#include "rebulkpch.h"
+
+namespace Rbk
+{
+	static std::vector<char> ReadFile(const std::string& filename)
+	{
+		std::ifstream file(filename, std::ios::ate | std::ios::binary);
+
+		if (!file.is_open()) {
+			throw std::runtime_error("failed to open file!");
+		}
+
+		size_t fileSize = (size_t)file.tellg();
+		std::vector<char> buffer(fileSize);
+
+		file.seekg(0);
+		file.read(buffer.data(), fileSize);
+
+		file.close();
+
+		return buffer;
+	}
+}
