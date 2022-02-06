fab436b329da72fd67b225cdbde88ba07ff0a89c
Guillaume P <gcconantc@gmail.com>
2022-02-06 23:39:02 +0100

    display texture by mesh

--- src/Rebulk/Renderer/Mesh.h
+++ src/Rebulk/Renderer/Mesh.h
@@ -4,9 +4,9 @@
 namespace Rbk
 {
 	struct UniformBufferObject {
-		glm::mat4 model;
-		glm::mat4 view;
-		glm::mat4 proj;
+		alignas(16) glm::mat4 model;
+		alignas(16) glm::mat4 view;
+		alignas(16) glm::mat4 proj;
 	};
 
 	struct Vertex {
