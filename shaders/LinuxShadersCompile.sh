dfee028d48594c135e35b367622908f884869b61
Guillaume P <gcconantc@gmail.com>
2022-02-08 13:46:49 +0100

    fix typo

--- shaders/LinuxShadersCompile.sh
+++ shaders/LinuxShadersCompile.sh
@@ -1,3 +1,3 @@
-../venbor/bin/glslc shader.vert -o spv/vert.spv
-../venbor/bin/glslc shader.frag -o spv/frag.spv
+../vendor/bin/glslc shader.vert -o spv/vert.spv
+../vendor/bin/glslc shader.frag -o spv/frag.spv
 
