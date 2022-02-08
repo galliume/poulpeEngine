d6cfbe139b81f72a86d6d81d785bff8d67fd57b3
Guillaume P <gcconantc@gmail.com>
2022-02-09 00:09:17 +0100

    WIP

--- src/Rebulk/Manager/RenderManager.cpp
+++ src/Rebulk/Manager/RenderManager.cpp
@@ -53,11 +53,12 @@ namespace Rbk
 		ubo3.proj = glm::perspective(glm::radians(45.0f), width / (float)height, 0.1f, 10.0f);
 		ubo3.proj[1][1] *= -1;
 
-		//AddMesh("mesh/viking/viking_room.obj", "mesh/viking/viking_room.png", ubo3);
+		AddMesh("mesh/viking/viking_room.obj", "mesh/viking/viking_room.png", ubo3);
+		AddMesh("mesh/moon/moon.obj", "mesh/moon/diffuse.jpg", ubo2);
+		//AddMesh("mesh/kitty/kitty.obj", "mesh/kitty/diffuse.jpg", ubo3);
 		//AddMesh("mesh/moon/moon.obj", "mesh/moon/diffuse.jpg", ubo2);
 		//AddMesh("mesh/moon/moon.obj", "mesh/moon/diffuse.jpg", ubo2);
-		//AddMesh("mesh/moon/moon.obj", "mesh/moon/diffuse.jpg", ubo2);
-		AddMesh("mesh/backpack/backpack.obj", "mesh/backpack/diffuse.png", ubo2);
+		//AddMesh("mesh/backpack/backpack.obj", "mesh/backpack/diffuse.png", ubo2);
 	}
 
 	void RenderManager::AddMesh(const char* path, const char* texturePath, UniformBufferObject ubo)
