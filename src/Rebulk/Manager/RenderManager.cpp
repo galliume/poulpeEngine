641a8c604d4148043f2b8806d0118fbe1b58e7b2
Guillaume P <gcconantc@gmail.com>
2022-02-13 22:15:45 +0100

    working movement
    Former-commit-id: fd9005a7002e30f39305d4b52256d1019d84ab16 [formerly 807da250b66a9d5138e03c67863119e2a2b751a4] [formerly 63dd665c60cce802b2f9782f336a854198d93a72 [formerly 63dd665c60cce802b2f9782f336a854198d93a72 [formerly 7874e29936ba33f7106757ba6cd7313ac9ed300c]]]
    Former-commit-id: 63dd665c60cce802b2f9782f336a854198d93a72
    Former-commit-id: 771f53f532ee9cbca2e1b7618d982a2bd363b4b9
    Former-commit-id: b14d6ccc69f6f512aa373472077445186b15dcd5

--- src/Rebulk/Manager/RenderManager.cpp
+++ src/Rebulk/Manager/RenderManager.cpp
@@ -27,40 +27,27 @@ namespace Rbk
 		auto fragShaderCode = ReadFile("shaders/spv/frag.spv");
 
 		AddShader("main", vertShaderCode, fragShaderCode);
+		int width, height;
+		glfwGetWindowSize(m_Window, &width, &height);
 
-		UniformBufferObject ubo{};
-		ubo.model = glm::mat4(1.0f);
-		ubo.model *= glm::translate(glm::mat4(1.0f), glm::vec3(-1.0f, 0.0f, 0.0f));
-		ubo.view = glm::mat4(1.0f);
+		//AddMesh("mesh/minecraft/Grass_Block.obj", "minecraft_grass", minecraftGrass);
+		//AddMesh("mesh/moon/moon.obj", "diffuse_moon", ubo2);
+		//AddMesh("mesh/backpack/backpack.obj", "diffuse_backpack", ubo3, false);
+		//AddMesh("mesh/viking/viking_room.obj", "viking_room", ubo);
+		//AddMesh("mesh/backpack/backpack.obj", "diffuse_backpack", ubo3, false);
 
-		UniformBufferObject ubo2{};
-		ubo2.model = glm::mat4(1.0f);
-		ubo2.model = glm::scale(ubo2.model, glm::vec3(0.3f, 0.3f, 0.3f));
-		ubo2.view = glm::mat4(1.0f);
-
-		UniformBufferObject ubo3{};
-		ubo3.model = glm::mat4(1.0f);
-		ubo3.model = glm::scale(ubo3.model, glm::vec3(0.2f, 0.2f, 0.2f));
-		ubo3.model *= glm::translate(glm::mat4(1.0f), glm::vec3(-6.0f, 0.0f, 0.0f));
-		ubo3.view = glm::mat4(1.0f);	
+		glm::vec3 pos1 = glm::vec3(0.5f, 0.5f, 0.5f);
+		glm::vec3 pos2 = glm::vec3(-0.5f, -0.5f, -0.5f);
+		glm::vec3 pos3 = glm::vec3(0.1f, -0.8f, 0.0f);
 
 		AddTexture("viking_room", "mesh/viking/viking_room.png");
-		AddTexture("diffuse_backpack",  "mesh/backpack/diffuse.png");
+		AddTexture("diffuse_backpack", "mesh/backpack/diffuse.png");
 		AddTexture("diffuse_moon", "mesh/moon/diffuse.jpg");
 		AddTexture("minecraft_grass", "mesh/minecraft/Grass_Block_TEX.png");
 
-		UniformBufferObject minecraftGrass{};
-		minecraftGrass.model = glm::mat4(1.0f);
-		minecraftGrass.model *= glm::scale(glm::mat4(1.0f), glm::vec3(0.4f, 0.4f, 0.4f));		
-		//minecraftGrass.model *= glm::rotate(glm::mat4(1.0f), glm::radians(180.0f), glm::vec3(0.0f, 0.0f, 1.0f));
-		minecraftGrass.view = glm::mat4(1.0f);
-
-		//AddMesh("mesh/minecraft/Grass_Block.obj", "minecraft_grass", minecraftGrass);
-		AddMesh("mesh/moon/moon.obj", "diffuse_moon", ubo2);
-		//AddMesh("mesh/backpack/backpack.obj", "diffuse_backpack", ubo3, false);
-		AddMesh("mesh/viking/viking_room.obj", "viking_room", ubo);
-		//AddMesh("mesh/backpack/backpack.obj", "diffuse_backpack", ubo3, false);
-		AddMesh("mesh/backpack/backpack.obj", "diffuse_backpack", ubo3, false);
+		AddMesh("mesh/backpack/backpack.obj", "diffuse_backpack", pos1, false);
+		AddMesh("mesh/backpack/backpack.obj", "diffuse_backpack", pos2, false);
+		AddMesh("mesh/backpack/backpack.obj", "diffuse_backpack", pos3, false);
 	}
 
 	void RenderManager::AddCamera(Camera* camera)
@@ -68,13 +55,13 @@ namespace Rbk
 		m_Renderer->AddCamera(camera);
 	}
 
-	void RenderManager::AddMesh(const char* path, const char* textureName, UniformBufferObject ubo, bool shouldInverseTextureY)
+	void RenderManager::AddMesh(const char* path, const char* textureName, glm::vec3 pos, bool shouldInverseTextureY)
 	{
 		Mesh mesh;
 		Rbk::TinyObjLoader::LoadMesh(mesh, path, shouldInverseTextureY);
 
 		//@todo Add MeshManager
-		m_Renderer->AddMesh(mesh, textureName, ubo);
+		m_Renderer->AddMesh(mesh, textureName, pos);
 	}
 
 	void RenderManager::AddTexture(const char* name, const char* path)
--- src/Rebulk/Renderer/Adapter/IRendererAdapter.h
+++ src/Rebulk/Renderer/Adapter/IRendererAdapter.h
@@ -12,7 +12,7 @@ namespace Rbk
 		virtual void AddShader(std::string name, std::vector<char> vertexShaderCode, std::vector<char> fragShaderCode) = 0;
 		virtual void AddCamera(Camera* camera) = 0;
 		virtual void AddTexture(const char* name, const char* path) = 0;
-		virtual void AddMesh(Rbk::Mesh mesh, const char* textureName, UniformBufferObject ubo) = 0;
+		virtual void AddMesh(Rbk::Mesh mesh, const char* textureName, glm::vec3 pos) = 0;
 		virtual void PrepareDraw() = 0;
 		virtual void Draw() = 0;
 		virtual void Destroy() = 0;
