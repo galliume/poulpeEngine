b5a80ecfbe9e7885af458475df94f3c0cf62bcc5
Guillaume P <gcconantc@gmail.com>
2022-02-15 19:51:04 +0100

    update vulkan to 1.3.204 version
    Former-commit-id: d24b1bd9ae58fcaff1b4d1a08b407376605f2b25 [formerly f0b7fe695ac4d13abada130d6165ef2b1952cfa6] [formerly 3f94c35d3558c808937454e7746cd5dcea529807 [formerly 3f94c35d3558c808937454e7746cd5dcea529807 [formerly 53b669c2f18a8029445570c40d2ce08137dc6dc1]]]
    Former-commit-id: 3f94c35d3558c808937454e7746cd5dcea529807
    Former-commit-id: 7445353343ae67501d9ab8db50cb8b59ed8e1a78
    Former-commit-id: d7265b0a1558ceab720a3441e0aaf252f9f01e6b

--- shaders/WindowsShadersCompile.bat
+++ shaders/WindowsShadersCompile.bat
@@ -1,2 +1,2 @@
-C:\VulkanSDK\1.2.198.1\Bin\glslc.exe shaders/shader.vert -o shaders/spv/vert.spv
-C:\VulkanSDK\1.2.198.1\Bin\glslc.exe shaders/shader.frag -o shaders/spv/frag.spv
+glslc.exe shaders/shader.vert -o shaders/spv/vert.spv
+glslc.exe shaders/shader.frag -o shaders/spv/frag.spv
