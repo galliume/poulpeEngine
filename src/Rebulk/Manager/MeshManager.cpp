c71926e1b82235b28d5fc25eeded6f80badfe98f
Guillaume P <gcconantc@gmail.com>
2022-02-24 23:43:41 +0100

    infinite reverse projection

--- src/Rebulk/Manager/MeshManager.cpp
+++ src/Rebulk/Manager/MeshManager.cpp
@@ -34,33 +34,24 @@ namespace Rbk
 		UniformBufferObject ubo;
 		ubo.model = glm::mat4(1.0f);
 		ubo.model = glm::translate(ubo.model, pos);
-		//ubo.model = glm::scale(ubo.model, glm::vec3(0.05f, 0.05f, 0.05f));
+		ubo.model = glm::scale(ubo.model, glm::vec3(0.05f, 0.05f, 0.05f));
 		
 		float fovy = glm::radians(60.0f);
 		float f = 100.0f;
 		float n = 0.1f;
 		float s = 2560.0f / 1440.0f;
-		float g = 1.0f / std::tan(fovy * 0.5f);
 		float k = f / (f - n);
 
-		glm::mat4 frustumProj = glm::mat4(
-			g / s, 0.0f, 0.0f, 0.0f,
-			0.0f, g, 0.0f, 0.0f,
-			0.0f, 0.0f, k, -n * k,
-			0.0f, 0.0f, 1.0f, 0.0f
-		);
+		float g = 1.0f / tan(fovy * 0.5f);
+		float e = 0.0f;
 
+		glm::mat4 frustumProj = glm::mat4(0.0f);
+		frustumProj[0][0] = g / s;
+		frustumProj[1][1] = g;
+		frustumProj[2][2] = e;
+		frustumProj[2][3] = n * (1.0f - e);
+		frustumProj[3][3] = 1.0f;
 
-		//T const tanHalfFovy = tan(fovy / static_cast<T>(2));
-		//Result[0][0] = static_cast<T>(1) / (aspect * tanHalfFovy);
-		//Result[1][1] = static_cast<T>(1) / (tanHalfFovy);
-		//Result[2][2] = zFar / (zNear - zFar);
-		//Result[2][3] = -static_cast<T>(1);
-		//Result[3][2] = -(zFar * zNear) / (zFar - zNear);
-
-		//frustumProj[1][1] *= -1;
-		//glm::mat4 projection;
-		//ubo.proj = glm::perspective(glm::radians(60.0f), m_Renderer->GetSwapChainExtent().width / (float)m_Renderer->GetSwapChainExtent().height, 0.1f, 100.0f);
 		ubo.proj = frustumProj;
 		//ubo.proj[1][1] *= -1;
 		
