5d656525e09da29c4040b97a01fd22dedaa8bab4
Guillaume P <gcconantc@gmail.com>
2022-06-18 00:53:23 +0200

    WIP broken
    Former-commit-id: adada75938e372aa097c86f63741cb905afb85ad [formerly 03cd93055855016767c4c3cd52192751ec699a19] [formerly c829dc519306f55adca8c8bb0a71c1e88b9ce025 [formerly 5b57f1488cf76dcbed07b8f5e6a9dd772a69e632]] [formerly 4935bf9569a71df5a2887cd9916182027da6ee86 [formerly 585ddfdeb7605d103f54e27b13bfb3f2fc27eb1c] [formerly 5b57f1488cf76dcbed07b8f5e6a9dd772a69e632 [formerly 5b57f1488cf76dcbed07b8f5e6a9dd772a69e632 [formerly 7bad1c902adbc50909777b1f6ebb87bb81d95549]]]]
    Former-commit-id: 56572143bd7239635ba393e3915d46240b59cbbc [formerly e82ca1ed1b1f01197e368be0be514f70ab104b46] [formerly 5ec425411c4549d228dd0f0cf09fe2a2014082af [formerly 1a7d1aca603af8c14a91659b52986fc36162e091]]
    Former-commit-id: 5ec425411c4549d228dd0f0cf09fe2a2014082af
    Former-commit-id: d4f3ac8eacebbc62b640a7ee781ea5aaafa24095
    Former-commit-id: 08378f56a41d6bc23bcef34d5d81314b45f69f57

--- src/Rebulk/Component/Mesh.cpp
+++ src/Rebulk/Component/Mesh.cpp
@@ -3,5 +3,55 @@
 
 namespace Rbk
 {
+    Mesh::Mesh() : Entity(), Drawable() { }
 
+    void Mesh::Init(
+         std::string name,
+         std::string path,
+         std::vector<std::string> textureNames,
+         std::string shader,
+         glm::vec3 pos,
+         glm::vec3 scale,
+         glm::vec3 axisRot,
+         float rotAngle,
+         bool shouldInverseTextureY)
+    {
+        //@todo move out of Mesh
+        if (!std::filesystem::exists(path)) {
+            Rbk::Log::GetLogger()->critical("mesh file {} does not exits.", path);
+            throw std::runtime_error("error loading a mesh file.");
+        }
+
+        std::vector<Data> listData = Rbk::TinyObjLoader::LoadData(path, shouldInverseTextureY);
+        //end todo
+
+        for (int i = 0; i < listData.size(); i++) {
+
+            uint32_t textureIndex = listData[i].materialId;
+            std::string id = name + '_' + textureNames[textureIndex] + '_' + std::to_string(i);
+
+            m_Name = id;
+            m_Texture = textureNames[textureIndex];
+            m_Shader = shader;
+
+            glm::mat4 view = glm::mat4(1.0f);
+
+            UniformBufferObject ubo;
+            ubo.model = glm::mat4(1.0f);
+            ubo.model = glm::translate(ubo.model, pos);
+            ubo.model = glm::scale(ubo.model, scale);
+
+            if (rotAngle != 0.0f) {
+                ubo.model = glm::rotate(ubo.model, glm::radians(rotAngle), axisRot);
+            }
+
+            ubo.view = glm::translate(view, glm::vec3(0.0f, 0.0f, 0.0f));
+            m_Ubos.emplace_back(ubo);
+        }
+    }
+
+    Mesh::~Mesh()
+    {
+
+    }
 }
