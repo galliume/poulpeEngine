fea8ddb6abbd273d648a289d8c997eac1df17de9
Guillaume P <gcconantc@gmail.com>
2022-06-18 22:47:33 +0200

    working threading pool for vulkan rendering, still not very fast
    Former-commit-id: acb67a57c7748b606dfdc132529cacdcf70ac010 [formerly f228e8b49239e2035f8669664bdd9bdbecaf02c3] [formerly 7df8d650ff9e186aac605adf1a4070628536f2ee] [formerly 7c9044663357b9bcc6f5d67f4de48adde2d3a5e5 [formerly 406105c6497c4f3bbf138a283aca7c589cb60aff] [formerly 7df8d650ff9e186aac605adf1a4070628536f2ee [formerly 7df8d650ff9e186aac605adf1a4070628536f2ee [formerly 969b96797cadb4335ee737257fc5f200e29bb70a]]]]
    Former-commit-id: db9d8dd1e88a90fd53c7cf96cb7356960ce45528 [formerly 366cd5d12da4d860448b4ddf3d852a7c72ab029b] [formerly 0dcdc5714dd6ef7e67b55acc98363214147b95a1 [formerly 3d64f3721d511a8303807ed2b8c198c86eb816b5]]
    Former-commit-id: 0dcdc5714dd6ef7e67b55acc98363214147b95a1
    Former-commit-id: f53b075f626a0cd63cc4d0c30f55cacf15c0a227
    Former-commit-id: b625d010b908d08439f1f0806d368145547e3a1f

--- src/Rebulk/Core/ThreadPool.cpp
+++ src/Rebulk/Core/ThreadPool.cpp
@@ -5,7 +5,9 @@ namespace Rbk
 {
     void ThreadPool::Start()
     {
-        const uint32_t numThreads = 4;// std::thread::hardware_concurrency();
+        m_ShouldTerminate = false;
+
+        const uint32_t numThreads = 4;//std::thread::hardware_concurrency();
         m_Threads.resize(numThreads);
 
         for (uint32_t i = 0; i < numThreads; i++) {
@@ -51,7 +53,22 @@ namespace Rbk
             std::unique_lock<std::mutex> lock(m_QueueMutex);
             poolbusy = m_Jobs.empty();
         }
+
         return poolbusy;
+    }
+
+    void ThreadPool::Join()
+    {
+        {
+            std::unique_lock<std::mutex> lock(m_QueueMutex);
+            m_ShouldTerminate = true;
+        }
+
+        m_MutexCondition.notify_all();
+
+        for (std::thread& activeThread : m_Threads) {
+            activeThread.join();
+        }
     }
 
     void ThreadPool::Stop()
