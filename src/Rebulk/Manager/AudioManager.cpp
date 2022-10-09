cd574f40b7ff78fb0ec77df8c46c485d3e0d8782
Guillaume P <gcconantc@gmail.com>
2022-10-09 22:53:36 +0200

    Improve a little sound management

--- src/Rebulk/Manager/AudioManager.cpp
+++ src/Rebulk/Manager/AudioManager.cpp
@@ -26,25 +26,74 @@ namespace Rbk
     void AudioManager::LoadAmbient()
     {
         m_AmbientSounds.emplace_back("assets/sounds/ambient/wind__artic__cold-6195.mp3");
+        m_AmbientSounds.emplace_back("assets/sounds/ambient/67884__benboncan__lake-waves-2.wav");
 
         StartAmbient();
     }
 
-    void AudioManager::StartAmbient()
+    void AudioManager::StartAmbient(int index)
     {
+        if (index > m_AmbientSounds.size() - 1 || 0 > index) {
+            return Rbk::Log::GetLogger()->warn("Ambient sound index {} does not exists.", index);
+        }
+
+        if (State::PLAY == m_State) {
+            StopAmbient();
+        }
+
         ma_result result;
 
-        result = ma_sound_init_from_file(&m_Engine, m_AmbientSounds[0].c_str(), 0, NULL, NULL, &m_AmbientSound);
-        if (result != MA_SUCCESS) {
-            Rbk::Log::GetLogger()->warn("Cannot init sound {}", m_AmbientSounds[0].c_str());
+        ma_uint32 flags = MA_SOUND_FLAG_ASYNC;
+        result = ma_sound_init_from_file(&m_Engine, m_AmbientSounds[index].c_str(), flags, NULL, NULL, &m_AmbientSound);
+
+        if (m_Looping) {
+            ma_sound_set_looping(&m_AmbientSound, true);
         }
-        
-        ma_sound_start(&m_AmbientSound);
+
+        if (result != MA_SUCCESS) {
+            Rbk::Log::GetLogger()->warn("Cannot init sound {}", m_AmbientSounds[index].c_str());
+        }
+
+        if (MA_SUCCESS == ma_sound_start(&m_AmbientSound)) {
+            m_AmbientSoundIndex = index;
+            m_State = State::PLAY;
+        } else {
+            m_State = State::ERR;
+        }
     }
 
     void AudioManager::StopAmbient()
     {
-        ma_sound_stop(&m_AmbientSound);
+        if (State::PLAY != m_State) {
+            return;
+        }
+
+        if (MA_SUCCESS == ma_sound_stop(&m_AmbientSound)) {
+            m_State = State::STOP;
+        } else {
+            m_State = State::ERR;
+        }
+
         ma_sound_uninit(&m_AmbientSound);
+    }
+
+    std::string const AudioManager::GetState()
+    {
+        if (State::PLAY == m_State) {
+            return "playing";
+        } else if (State::STOP == m_State) {
+            return "stoped";
+        } else if (State::ERR == m_State) {
+            return "an error occured";
+        }
+    }
+
+    void AudioManager::ToggleLooping()
+    {
+        if (ma_sound_is_looping(&m_AmbientSound)) {
+            ma_sound_set_looping(&m_AmbientSound, false);
+        } else {
+            ma_sound_set_looping(&m_AmbientSound, true);
+        }
     }
 }
--- src/Rebulk/Manager/AudioManager.h
+++ src/Rebulk/Manager/AudioManager.h
@@ -6,19 +6,31 @@ namespace Rbk
 {
     class AudioManager
     {
+        enum class State {
+            STOP,
+            PLAY,
+            ERR
+        };
+
     public:
         AudioManager();
         ~AudioManager();
         void LoadAmbient();
 
-        void StartAmbient();
+        void StartAmbient(int index = 0);
         void StopAmbient();
         std::vector<std::string> const GetAmbientSound() { return m_AmbientSounds; };
+        std::string const GetState();
+        int const GetAmbientSoundIndex() { return m_AmbientSoundIndex; };
+        std::string const GetCurrentAmbientSound() { return m_AmbientSounds[m_AmbientSoundIndex]; };
+        void ToggleLooping();
 
     private:
         ma_engine m_Engine;
         ma_sound m_AmbientSound;
-
+        State m_State;
+        int m_AmbientSoundIndex = 0;
+        bool m_Looping = true;
         std::vector<std::string> m_AmbientSounds {};
     };
 }
