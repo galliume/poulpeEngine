a3cf3f8326217d3bfe626ca13edb68abd4c2efb4
Guillaume PERCEPIED <percepiedg@gmail.com>
2023-02-21 23:44:16 +0100

    Tooling : Github Actions (#95)

--- bin/cmakeCmd.sh
+++ bin/cmakeCmd.sh
@@ -25,16 +25,7 @@ configure() {
 	echo "Moving to ${CMAKE_BUILD_DIR}"
 	cd "./${CMAKE_BUILD_DIR}"
 
-	echo "Starting configuration with options: "
-	echo "Build dir: ${CMAKE_BUILD_DIR}/${CMAKE_BUILD_TYPE}"
-	echo "Building from: ../${CMAKE_BUILD_DIR}"
-	echo "CMAKE_CXX_COMPILER: ${CMAKE_CXX_COMPILER}"
-	echo "CMAKE_C_COMPILER: ${CMAKE_C_COMPILER}"
-	echo "CMAKE_BUILD_TYPE: ${CMAKE_BUILD_TYPE}"
-	echo "CMAKE_J: ${CMAKE_J}"
-	echo "USE_NINJA: ${USE_NINJA}" 
-	echo "OS: $OS"
-	
+
 	NINJA=""
 	CONFIG_TYPE="-DCMAKE_CONFIGURATION_TYPES=Debug;Release;RelWithDebInfo"
 	BUILD_TYPE="-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
@@ -45,29 +36,43 @@ configure() {
 	if [[ "$OS" == 2 ]]; then
 		TOOLSET=""
 	fi
-	
+		
 	if [ $USE_NINJA = true ]; then
-		NINJA="-GNinja"
+		NINJA="-G Ninja"
+		TOOLSET=""		
 		
 		if ! [[ "$OS" == 2 ]]; then
-			NINJA=""
+			
 			echo "Ninja config not ready for Visual Studio, Linux only, yet."
 			#TOOLSET="-DCMAKE_GENERATOR_TOOLSET=v142"
 			#cmd "C:\Program Files (x86)\Microsoft Visual Studio\2019\Professional\VC\Auxiliary\Build\vcvarsall.bat" x64
 			#"C:\Program Files\Git\bin\sh.exe" --login -i
 		fi
 	fi	
+
+	echo "Starting configuration with options: "
+	echo "Build dir: ${CMAKE_BUILD_DIR}/${CMAKE_BUILD_TYPE}"
+	echo "Building from: ../${CMAKE_BUILD_DIR}"
+	echo "${CXX_COMPILER}"
+	echo "${C_COMPILER}"
+	echo "${BUILD_TYPE}"
+	echo "CMAKE_J: ${CMAKE_J}"
+	echo "USE_NINJA: ${USE_NINJA} ${NINJA}" 
+	echo "TOOLSET: ${TOOLSET}"
+	echo "OS: $OS"
 	
 	cmake ${NINJA} ${CONFIG_TYPE} ${BUILD_TYPE} ${CXX_COMPILER} ${C_COMPILER} ${TOOLSET} ..
 
-	echo "Configuration done"
+	RESULT=$?
+	
+	echo "Configuration done with code: ${RESULT}"
 
 	#Just to be sure to be on the docking branch
 	echo "Switching to ImGui docking branch"
 	cd "../vendor/imgui"
 	git checkout docking
 	
-	return 0
+	exit $((RESULT+0))
 }
 
 build() {
@@ -83,10 +88,10 @@ build() {
 		
 		cd "../"
 		
-		return 0
+		exit $((RESULT+0))
 	else
 		echo "Build dir [${CMAKE_BUILD_DIR}] does not exists"
-		return -1
+		exit -1
 	fi
 }
 
@@ -98,10 +103,25 @@ run() {
 	if ! [[ "$OS" == 2 ]]; then
 		RBK_BIN="Rebulkan.exe"
 	fi
-
-	sleep 2 #why some segmentation fault ? too slow to finalise building ?
 	
-	"./${CMAKE_BUILD_DIR}/${CMAKE_BUILD_TYPE}/${RBK_BIN}"
+	RBK_PATH="./${CMAKE_BUILD_DIR}/${CMAKE_BUILD_TYPE}/${RBK_BIN}"
 	
-	return 0
+	if [ -f $RBK_PATH ] ; then
+		RBK_PATH
+		RESULT=$?
+		exit $((RESULT+0))		
+	else
+		RBK_PATH="./${CMAKE_BUILD_DIR}/${RBK_BIN}"
+	fi
+	
+	if [ -f $RBK_PATH ] ; then
+		RBK_PATH
+		RESULT=$?
+		exit $((RESULT+0))
+	else
+		echo "Can't find Rebulkan binary"
+		exit -1
+	fi
+	
+	exit 0
 }
