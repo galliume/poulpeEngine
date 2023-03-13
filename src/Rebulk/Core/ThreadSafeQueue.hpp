6b284f9bd1cad8df57d5af95a263bc10a1c78d9c
Guillaume PERCEPIED <guillaume.percepied@siemens.com>
2023-03-13 15:40:16 +0100

    wip
    Former-commit-id: 0b97eab15d449984984c7e7cb4060627baa47b59 [formerly 12d57886bb03648a1c943ae4a5044db7085183db] [formerly 0a37098d87860035027990afcd605247c88eca4b [formerly 77258796d29605943849d40491c320b73d2482b3]] [formerly cc9a7a566f817a80145299a03c2a5b19e14315be [formerly 5d3ba9d1bdc3c8c54bd8468b5da5001052951562] [formerly 77258796d29605943849d40491c320b73d2482b3 [formerly 77258796d29605943849d40491c320b73d2482b3 [formerly 16ee72ceb9d10731935b942892783943f3b2540f]]]]
    Former-commit-id: f7cde9386f2dcd8f10cf22df14d38c83a51068af [formerly ae3404131c8cbe67f6d27e24d679e1c5d42e853d] [formerly dd9373a95dd30c3ff9eb11c494629d7ff8458855 [formerly 363f0c9e7210d0902c2dfd5695af217a6cb3eaa9]]
    Former-commit-id: dd9373a95dd30c3ff9eb11c494629d7ff8458855
    Former-commit-id: 137198efe45c70afbb3b02143f52f1e725793a89
    Former-commit-id: c0f588affd8ed8f337427d657ca48740d9bcebe2

--- src/Rebulk/Core/ThreadSafeQueue.hpp
+++ src/Rebulk/Core/ThreadSafeQueue.hpp
@@ -12,62 +12,81 @@ namespace Rbk
 
         void Push(T newValue)
         {
-            std::shared_ptr<T>data(std::make_shared<T>(std::move(newValue)));
-            std::lock_guard<std::mutex> lock(m_Mutex);
-            m_DataQueue.push(data);
-            m_DataCond.notify_one();
+            {
+                std::lock_guard<std::mutex> lock(m_PushMutex);
+                std::shared_ptr<T>data(std::make_shared<T>(std::move(newValue)));
+                m_DataQueue.push(data);
+                m_DataCond.notify_one();
+            }
         }
 
         void WaitAndPop(T& value)
         {
-            std::unique_lock<std::mutex> lock(m_Mutex);
-            m_DataCond.wait(lock, [=, this] { return !m_DataQueue.empty(); });
+            {
+                std::unique_lock<std::mutex> lock(m_WaitMutex);
+                m_DataCond.wait(lock, [=, this] { return !m_DataQueue.empty(); });
 
-            value = std::move(*m_DataQueue.front());
-            m_DataQueue.pop();
+                value = std::move(*m_DataQueue.front());
+                m_DataQueue.pop();
+            }
         }
 
         std::shared_ptr<T> WaitAndPop()
         {
-            std::unique_lock<std::mutex> lock(m_Mutex);
-            m_DataCond.wait(lock, [=, this] { return !m_DataQueue.empty(); });
+            {
+                std::unique_lock<std::mutex> lock(m_WaitMutex);
+                m_DataCond.wait(lock, [=, this] { return !m_DataQueue.empty(); });
 
-            std::shared_ptr<T> res = m_DataQueue.front();
-            m_DataQueue.pop();
+                std::shared_ptr<T> res = m_DataQueue.front();
+                m_DataQueue.pop();
+            }
 
             return res;
         }
 
         bool TryPop(T& value)
         {
-            std::lock_guard<std::mutex> lock(m_Mutex);
-            if (m_DataQueue.empty()) return false;
+            {
+                std::lock_guard<std::mutex> lock(m_PopMutex);
+                if (m_DataQueue.empty()) return false;
 
-            value = std::move(*m_DataQueue.front());
-            m_DataQueue.pop();
+                value = std::move(*m_DataQueue.front());
+                m_DataQueue.pop();
+            }
 
             return true;
         }
 
         std::shared_ptr<T> TryPop()
         {
-            std::lock_guard<std::mutex> lock(m_Mutex);
-            if (m_DataQueue.empty()) return false;
+            {
+                std::lock_guard<std::mutex> lock(m_PopMutex);
+                if (m_DataQueue.empty()) return false;
 
-            std::shared_ptr<T> res = m_DataQueue.front();
-            m_DataQueue.pop();
+                std::shared_ptr<T> res = m_DataQueue.front();
+                m_DataQueue.pop();
+            }
 
             return res;
         }
 
         bool Empty()
         {
-            std::lock_guard<std::mutex> lock(m_Mutex);
-            return m_DataQueue.empty();
+            bool isEmpty{ false };
+
+            {
+                std::lock_guard<std::mutex> lock(m_Mutex);
+                isEmpty = m_DataQueue.empty();
+            }
+
+            return isEmpty;
         }
 
     private:
         mutable std::mutex m_Mutex;
+        mutable std::mutex m_PushMutex;
+        mutable std::mutex m_WaitMutex;
+        mutable std::mutex m_PopMutex;
         std::queue<std::shared_ptr<T>> m_DataQueue;
         std::condition_variable m_DataCond;
     };
