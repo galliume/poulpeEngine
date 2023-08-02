5f3380a135bac162bc7e0e63cec5e442956f2600
Guillaume P <gcconantc@gmail.com>
2023-08-02 23:19:49 +0200

    update about, hide glfw window

--- src/Poulpe/GUI/Window.cpp
+++ src/Poulpe/GUI/Window.cpp
@@ -1,4 +1,5 @@
 #include "Poulpe/GUI/Window.hpp"
+#include "stb_image.h"
 
 namespace Poulpe
 {
@@ -6,28 +7,33 @@ namespace Poulpe
 
     void Window::Init(std::string_view windowTitle)
     {
-        const uint32_t WIDTH = 800;
-        const uint32_t HEIGHT = 600;
+        const uint32_t WIDTH = 2048;
+        const uint32_t HEIGHT = 1080;
 
         glfwInit();
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
         glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-        glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
+        //glfwWindowHint(GLFW_CONTEXT_CREATION_API, GLFW_NATIVE_CONTEXT_API);
         //glfwWindowHint(GLFW_DECORATED, GLFW_FALSE);
+#ifdef PLP_DEBUG_BUILD
+        glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
+#endif
 
         m_Window = glfwCreateWindow(WIDTH, HEIGHT, windowTitle.data(), nullptr, nullptr);
 
+        //GLFWimage images[1]; 
+        //images[0].pixels = stbi_load("./assets/mpoulpe.png", &images[0].width, &images[0].height, 0, 4);
+        //glfwSetWindowIcon(m_Window, 1, images); 
+        //stbi_image_free(images[0].pixels);
+
         glfwSetWindowSizeLimits(m_Window, 800, 600, 2048, 1080);
-        glfwMakeContextCurrent(m_Window);
-        glfwSwapInterval(false);
         
         int maxWidth  = GetSystemMetrics(SM_CXSCREEN);
         int maxHeight = GetSystemMetrics(SM_CYSCREEN);
+        
         glfwSetWindowMonitor(m_Window, NULL, (maxWidth/2)-(WIDTH/2), (maxHeight/2) - (HEIGHT/2), WIDTH, HEIGHT, GLFW_DONT_CARE);
-
-
         glfwSetWindowUserPointer(m_Window, this);
 
         glfwSetFramebufferSizeCallback(m_Window, [](GLFWwindow* glfwWindow, int width, int height) {
