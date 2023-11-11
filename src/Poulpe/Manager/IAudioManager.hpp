b1fbc1bb5f182b69b7f11cba7d891be3b7cfe414
Guillaume P <gcconantc@gmail.com>
2023-11-11 11:05:53 +0100

    fix sound selection

--- src/Poulpe/Manager/IAudioManager.hpp
+++ src/Poulpe/Manager/IAudioManager.hpp
@@ -1,6 +1,7 @@
 #pragma once
-#include <nlohmann/json.hpp>
+
 #include <miniaudio.h>
+#include <nlohmann/json.hpp>
 
 namespace Poulpe
 {
@@ -12,13 +13,13 @@ namespace Poulpe
         ~IAudioManager() = default;
 
         virtual void load(nlohmann::json config) = 0;
-        virtual void startSplash(int index = 0) = 0;
+        virtual void startSplash(unsigned int const index = 0) = 0;
         virtual void stopSplash() = 0;
-        virtual void startAmbient(int index = 0) = 0;
+        virtual void startAmbient(unsigned int const index = 0) = 0;
         virtual void stopAmbient() = 0;
         virtual std::vector<std::string> const getAmbientSound() = 0;
         virtual std::string const getState() = 0;
-        virtual int getAmbientSoundIndex() = 0;
+        virtual unsigned int getAmbientSoundIndex() = 0;
         virtual std::string getCurrentAmbientSound() = 0;
         virtual void toggleLooping() = 0;
     };
