0b2247790e173285284be1d74718392d920de76e
Guillaume PERCEPIED <guillaume.percepied@siemens.com>
2023-11-14 09:53:59 +0100

    clean shared ptr in App
    Former-commit-id: cf37eb6442ec73a8bd6883e5d4a1098bbc10a816 [formerly 237cd1d565335ae752bdf5370873cda9b987ae7e] [formerly 908a696de2e39d534cd47286d23503737b02db5c [formerly 1d330445add6dad68ed48f070b2d5d47c98176f7]] [formerly a91ca485e9db1506d41145a9d4e6ace8c00f6dfb [formerly 461acc357137487721483f7da6028a94e87a5c9b] [formerly 1d330445add6dad68ed48f070b2d5d47c98176f7 [formerly 1d330445add6dad68ed48f070b2d5d47c98176f7 [formerly d45a48382a884753844f3efb8ec42719ac69882d]]]]
    Former-commit-id: c442877efe8736c91052bf7619c9d890a1c191cd [formerly d68f89170788ef2313c9066b412b24b76a842cde] [formerly 35bdd6d03a134ebdff999ffa22f1a4f855a48ecc [formerly 9792334bb1e77470efa1354810166af952e0afcd]]
    Former-commit-id: 03620f1c829217d29c67dab85c3ddc66a24ad776 [formerly 35bdd6d03a134ebdff999ffa22f1a4f855a48ecc]
    Former-commit-id: 58b7f355bbef6a29e874d3ccb04dca621b82ef90
    Former-commit-id: a07825a03b5af8465f83a20fbaf78ed6737d2b8c

--- src/Poulpe/Application.hpp
+++ src/Poulpe/Application.hpp
@@ -7,16 +7,6 @@
 #include "GUI/Layer/VulkanLayer.hpp"
 #include "GUI/Window.hpp"
 
-#include "Manager/AudioManager.hpp"
-#include "Manager/ConfigManager.hpp"
-#include "Manager/DestroyManager.hpp"
-#include "Manager/EntityManager.hpp"
-#include "Manager/InputManager.hpp"
-#include "Manager/IRenderManager.hpp"
-#include "Manager/ShaderManager.hpp"
-#include "Manager/SpriteAnimationManager.hpp"
-#include "Manager/TextureManager.hpp"
-
 #include "Renderer/Adapter/VulkanAdapter.hpp"
 
 namespace Poulpe
@@ -31,14 +21,14 @@ namespace Poulpe
         void init();
         void run();
 
-        static  std::atomic<int> s_UnlockedFPS;
+        static std::atomic<int> s_UnlockedFPS;
 
     private:
         static Application* s_Instance;
 
-        std::shared_ptr<Poulpe::Window> m_Window;
-        std::shared_ptr<Poulpe::RenderManager> m_RenderManager;
-        std::shared_ptr<Poulpe::VulkanLayer> m_VulkanLayer;
+        std::unique_ptr<Poulpe::Window> m_Window;
+        std::unique_ptr<Poulpe::RenderManager> m_RenderManager;
+        std::unique_ptr<Poulpe::VulkanLayer> m_VulkanLayer;
 
         std::chrono::time_point<std::chrono::system_clock> m_StartRun;
     };
