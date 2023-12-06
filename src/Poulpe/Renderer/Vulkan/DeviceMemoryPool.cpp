01ddabc4175421a662fde2d024568aa5cc2cc2fc
Guillaume PERCEPIED <guillaume.percepied@siemens.com>
2023-12-06 17:25:13 +0100

    refacto device memory
    Former-commit-id: 9097e0880cf0fbafe7401fb6f66d16b86e8d86b1 [formerly 4b69fd63a1ca3c66b3f5054bec825697abf3837d] [formerly c91d63721087282d04c5c341b666898b0f35987c] [formerly 4ebb47bd35d6556726116adb116b159c9bd437e8 [formerly 51172f3727053734178affbaaabc078dd6a8658e] [formerly c91d63721087282d04c5c341b666898b0f35987c [formerly c91d63721087282d04c5c341b666898b0f35987c [formerly 2e21fbd24e54476126fcc3c766b7a8a5b7665245]]]]
    Former-commit-id: 3c2aa4c460feb09d4767d64c6d5a3c966e438623 [formerly dadbb36dcc32ae1cdce6b0b8f30307067b986bb5] [formerly 6e628d73953e525c0da89f04dddd473bf5463b7b [formerly c868944cf6dd518ffa26a71f09892e4a448d8a16]]
    Former-commit-id: 6e628d73953e525c0da89f04dddd473bf5463b7b
    Former-commit-id: bf6a4258987824dd69dd698ef4db3f38427fd748
    Former-commit-id: 845aa75caf207d9eac0b4669893c8c16f489382c

--- src/Poulpe/Renderer/Vulkan/DeviceMemoryPool.cpp
+++ src/Poulpe/Renderer/Vulkan/DeviceMemoryPool.cpp
@@ -3,74 +3,111 @@
 namespace Poulpe
 {
     DeviceMemoryPool::DeviceMemoryPool(VkPhysicalDeviceProperties2 deviceProperties,
-      VkPhysicalDeviceMaintenance3Properties maintenceProperties)
-        : m_DeviceProperties(deviceProperties), m_MaintenanceProperties(maintenceProperties)
+      VkPhysicalDeviceMaintenance3Properties maintenanceProperties,
+      VkPhysicalDeviceMemoryProperties memProperties)
+      : m_DeviceProperties(deviceProperties),
+        m_MaintenanceProperties(maintenanceProperties),
+        m_MemProperties(memProperties)
     {
-
+        m_MemoryAllocationSize.resize(m_MemProperties.memoryTypeCount);
     }
-    DeviceMemory* DeviceMemoryPool::get(VkDevice & device, VkDeviceSize size, uint32_t memoryType,
-      VkBufferUsageFlags usage, bool forceNew)
+
+    DeviceMemory* DeviceMemoryPool::get(VkDevice& device, VkDeviceSize size,
+      uint32_t memoryType, VkBufferUsageFlags usage, DeviceBufferType bufferType,
+      bool forceNew)
     {
         if (m_MemoryAllocationCount > m_DeviceProperties.properties.limits.maxMemoryAllocationCount) {
-            throw std::runtime_error("Max number of active allocation reached");
+          throw std::runtime_error("Max number of active allocation reached");
         }
 
-        VkDeviceSize maxSize;
+       unsigned int bufferSize;
+       VkDeviceSize maxBufferSize = m_MemProperties.memoryHeaps[m_MemProperties.memoryTypes[memoryType].heapIndex].size;
+       std::string bufferTypeDebug;
 
-        maxSize = m_MaintenanceProperties.maxMemoryAllocationSize / 6.f;
+       switch (bufferType) {
+           case DeviceBufferType::STORAGE:
+             bufferSize = m_MaintenanceProperties.maxMemoryAllocationSize;
+             bufferTypeDebug = "STORAGE";
+           break;
+           case DeviceBufferType::UNIFORM:
+             bufferSize = m_DeviceProperties.properties.limits.maxUniformBufferRange * m_MaxUBOS;
+             bufferTypeDebug = "UNIFORM";
+           break;
+           case DeviceBufferType::STAGING:
+             bufferSize = m_MaintenanceProperties.maxMemoryAllocationSize;
+             bufferTypeDebug = "STAGING";
+           break;
+           default:
+             throw std::runtime_error("Memory type unknown");
+      }
 
-        auto poolType = m_Pool.find(memoryType);
+      auto poolType = m_Pool.find(memoryType);
 
-        if (m_Pool.end() != poolType && !forceNew) {
-            auto poolUsage = poolType->second.find(usage);
-            if (poolType->second.end() != poolUsage) {
-                for (size_t i = 0; i < poolUsage->second.size(); ++i) {
-                    auto & dm = poolUsage->second.at(i);
-                    if (!dm->isFull() && dm->hasEnoughSpaceLeft(size)) {
-                        //PLP_DEBUG("memory allocation recycle: size {} type {} usage {} full {} space left {}", size, memoryType, usage, dm->IsFull(), dm->HasEnoughSpaceLeft(size));
-                        return dm.get();
-                    }
-                }
+      if (m_Pool.end() != poolType && !forceNew) {
+        auto poolUsage = poolType->second.find(usage);
+        if (poolType->second.end() != poolUsage) {
+          for (size_t i = 0; i < poolUsage->second.size(); ++i) {
+            auto& dm = poolUsage->second.at(i);
+            if (!dm->isFull() && dm->hasEnoughSpaceLeft(size)) {
+              PLP_DEBUG("DM REUSE: id {}, type {}, size {} type {} usage {} full {} space left {}", 
+                dm.get()->getID(), bufferTypeDebug, size, memoryType, usage, dm->isFull(), dm->getSpaceLeft());
+              return dm.get();
             }
-
-            if (m_MemoryAllocationSize + maxSize > m_MaintenanceProperties.maxMemoryAllocationSize) {
-                throw std::runtime_error("Max size of memory allocation reached");
-            }
-
-            m_Pool[memoryType][usage].emplace_back(std::make_unique<DeviceMemory>(device, memoryType, usage, maxSize));
-            m_MemoryAllocationCount += 1;
-            m_MemoryAllocationSize += maxSize;
-            //PLP_DEBUG("memory allocation creation: count {}, size {} type {} usage {} ", m_MemoryAllocationCount, m_MemoryAllocationSize, memoryType, usage);
-            return m_Pool[memoryType][usage].back().get();
-        } else {
-
-            if (m_MemoryAllocationSize + maxSize > m_MaintenanceProperties.maxMemoryAllocationSize) {
-                //throw std::runtime_error("Max size of memory allocation reached");
-                PLP_ERROR("Max size of memory allocation reached");
-            }
-
-            m_Pool[memoryType][usage].emplace_back(std::make_unique<DeviceMemory>(device, memoryType, usage, maxSize));
-            m_MemoryAllocationCount += 1;
-            m_MemoryAllocationSize += maxSize;
-            //PLP_DEBUG("memory allocation creation: count {}, size {} type {} usage {} ", m_MemoryAllocationCount, m_MemoryAllocationSize, memoryType, usage);
-            return m_Pool[memoryType][usage].back().get();
+          }
         }
+
+        if (m_MemoryAllocationSize.at(memoryType) + bufferSize > maxBufferSize) {
+            throw std::runtime_error("Max size of memory allocation reached");
+        }
+
+        m_Pool[memoryType][usage].emplace_back(std::make_unique<DeviceMemory>(device, memoryType, usage, bufferSize, m_DeviceMemoryCount));
+        m_MemoryAllocationCount += 1;
+        m_MemoryAllocationSize.at(memoryType) += bufferSize;
+        PLP_DEBUG("DM CREATION: id {}, type {}, id {}, size {} type {} usage {} ", 
+          m_DeviceMemoryCount, bufferTypeDebug, m_MemoryAllocationCount, bufferSize, memoryType, usage);
+        m_DeviceMemoryCount += 1;
+        return m_Pool[memoryType][usage].back().get();
+      } else {
+
+        if (m_MemoryAllocationSize.at(memoryType) + bufferSize > maxBufferSize) {
+          throw std::runtime_error("Max size of memory allocation reached");
+        }
+
+        m_Pool[memoryType][usage].emplace_back(std::make_unique<DeviceMemory>(device, memoryType, usage, bufferSize, m_DeviceMemoryCount));
+        m_MemoryAllocationCount += 1;
+        m_MemoryAllocationSize.at(memoryType) += bufferSize;
+        PLP_DEBUG("DM CREATION: id {}, type {}, id {}, size {} type {} usage {} ",
+          m_DeviceMemoryCount, bufferTypeDebug, m_MemoryAllocationCount, bufferSize, memoryType, usage);
+        m_DeviceMemoryCount += 1;
+        return m_Pool[memoryType][usage].back().get();
+      }
     }
 
     void DeviceMemoryPool::clear()
     {
-        PLP_WARN("MEMORY POOL CLEANING");
-        for (auto& memoryType : m_Pool) {
-            for (auto& usage : memoryType.second) {
-                for (auto& mem : usage.second) {
-                    mem->clear();
-                }
-            }
-        }
-        
-        m_Pool.clear();
+      PLP_WARN("MEMORY POOL CLEANING");
 
-        m_MemoryAllocationCount = 0;
-        m_MemoryAllocationSize = 0;
+      for (auto& memoryType : m_Pool) {
+        for (auto& usage : memoryType.second) {
+          for (auto& mem : usage.second) {
+              clear(mem.get());
+          }
+        }
+      }
+
+      m_Pool.clear();
+
+      m_MemoryAllocationCount = 0; //should be zero anyway
+      m_MemoryAllocationSize.clear();
+      m_DeviceMemoryCount = 0; //should be zero anyway
+    }
+
+    void DeviceMemoryPool::clear(DeviceMemory * deviceMemory)
+    {
+        m_MemoryAllocationSize.at(deviceMemory->getType()) -= deviceMemory->getSize();
+        m_MemoryAllocationCount -= 1;
+        m_DeviceMemoryCount -= 1;
+        deviceMemory->clear();
+        PLP_DEBUG("Clearing device memory id {}. {} left", deviceMemory->getID(), m_DeviceMemoryCount);
     }
 }
--- src/Poulpe/Renderer/Vulkan/DeviceMemoryPool.hpp
+++ src/Poulpe/Renderer/Vulkan/DeviceMemoryPool.hpp
@@ -6,22 +6,32 @@ namespace Poulpe
     class DeviceMemoryPool
     {
     public:
-        DeviceMemoryPool(VkPhysicalDeviceProperties2 deviceProperties,
-          VkPhysicalDeviceMaintenance3Properties maintenceProperties);
 
-        DeviceMemory* get(VkDevice & device, VkDeviceSize size, uint32_t memoryType, VkBufferUsageFlags usage,
-          bool forceNew = false);
+        enum class DeviceBufferType {
+          UNIFORM, STORAGE, STAGING
+        };
+
+        DeviceMemoryPool(VkPhysicalDeviceProperties2 deviceProperties,
+          VkPhysicalDeviceMaintenance3Properties maintenanceProperties,
+          VkPhysicalDeviceMemoryProperties memProperties);
+
+        DeviceMemory* get(VkDevice& device, VkDeviceSize size, uint32_t memoryType,
+          VkBufferUsageFlags usage, DeviceBufferType memType = DeviceBufferType::UNIFORM, bool forceNew = false);
 
         std::unordered_map<uint32_t, std::unordered_map<VkBufferUsageFlags,
           std::vector<std::unique_ptr<DeviceMemory>>>>* getPool() { return & m_Pool; }
 
+        void clear(DeviceMemory * deviceMemory);
         void clear();
 
     private:
         std::unordered_map<uint32_t, std::unordered_map<VkBufferUsageFlags, std::vector<std::unique_ptr<DeviceMemory>>>> m_Pool;
         VkPhysicalDeviceProperties2 m_DeviceProperties;
-        VkPhysicalDeviceMaintenance3Properties m_MaintenanceProperties;
+        [[maybe_unused]] VkPhysicalDeviceMaintenance3Properties m_MaintenanceProperties;
+        VkPhysicalDeviceMemoryProperties m_MemProperties;
         VkDeviceSize m_MemoryAllocationCount = 0;
-        VkDeviceSize m_MemoryAllocationSize = 0;
+        std::vector<VkDeviceSize> m_MemoryAllocationSize{0};
+        unsigned int m_DeviceMemoryCount{ 0 };
+        unsigned int const m_MaxUBOS{ 1000 };
     };
 }
