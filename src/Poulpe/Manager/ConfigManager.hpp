6c27d8a9203216b4f1235d1921a25469bea0eef3
Guillaume PERCEPIED <guillaume.percepied@siemens.com>
2023-12-21 13:44:39 +0100

    some cleaning

--- src/Poulpe/Manager/ConfigManager.hpp
+++ src/Poulpe/Manager/ConfigManager.hpp
@@ -9,23 +9,23 @@ namespace Poulpe
         ConfigManager();
         virtual ~ConfigManager() = default;
 
-        virtual nlohmann::json appConfig() override;
-        virtual nlohmann::json texturesConfig() override;
-        virtual nlohmann::json soundConfig() override;
-        virtual nlohmann::json shaderConfig() override;
+        virtual nlohmann::json appConfig() const override { return m_AppConfig; };
+        virtual std::vector<std::string> listLevels() const override;
+        virtual std::vector<std::string> listSkybox() const override;
         virtual nlohmann::json loadLevelData(std::string const & levelName) override;
-        virtual std::vector<std::string> listLevels() override;
-        virtual std::vector<std::string> listSkybox() override;
+        virtual nlohmann::json shaderConfig() const override { return m_ShaderConfig;  };
+        virtual nlohmann::json soundConfig() const override { return m_SoundConfig ;};
+        virtual nlohmann::json texturesConfig() const override { return m_TexturesConfig; };
 
     private:
+        std::string const m_LevelPath{ "config/levels/" };
+
         nlohmann::json m_AppConfig{};
-        nlohmann::json m_TexturesConfig{};
+        nlohmann::json m_EntityConfig{};
         nlohmann::json m_ShaderConfig{};
         nlohmann::json m_SoundConfig{};
-        nlohmann::json m_EntityConfig{};
+        nlohmann::json m_TexturesConfig{};
 
         std::mutex m_MutexRead;
-
-        std::string const m_LevelPath{ "config/levels/" };
     };
 }
