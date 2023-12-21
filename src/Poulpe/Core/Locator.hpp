d7e8351a86d9b2310950274daa3bc02592a0349f
Guillaume PERCEPIED <guillaume.percepied@siemens.com>
2023-12-21 13:16:57 +0100

    clean core

--- src/Poulpe/Core/Locator.hpp
+++ src/Poulpe/Core/Locator.hpp
@@ -10,23 +10,23 @@ namespace Poulpe {
     class Locator
     {
     public:
-        static ThreadPool* getThreadPool() { return m_ThreadPool.get(); }
-        static InputManager* getInputManager() { return m_InputManager.get(); }
         static CommandQueue* getCommandQueue() { return m_CommandQueue.get(); }
+        static InputManager* getInputManager() { return m_InputManager.get(); }
+        static ThreadPool* getThreadPool() { return m_ThreadPool.get(); }
+        
+        static void setCommandQueue(CommandQueue* commandQueue) {
+          m_CommandQueue = std::unique_ptr<CommandQueue>(commandQueue);}
+
+        static void setInputManager(InputManager* inputManager) {
+          m_InputManager = std::unique_ptr<InputManager>(inputManager); }
 
         //@todo inject with Interface not direct Impl
         static void setThreadPool(ThreadPool* threadPool) {
           m_ThreadPool = std::unique_ptr<ThreadPool>(threadPool); }
 
-        static void setInputManager(InputManager* inputManager) {
-          m_InputManager = std::unique_ptr<InputManager>(inputManager); }
-
-        static void setCommandQueue(CommandQueue* commandQueue) {
-          m_CommandQueue = std::unique_ptr<CommandQueue>(commandQueue);}
-
     private:
-        inline static std::unique_ptr<ThreadPool> m_ThreadPool;
-        inline static std::unique_ptr<InputManager> m_InputManager;
         inline static std::unique_ptr<CommandQueue> m_CommandQueue;
+        inline static std::unique_ptr<InputManager> m_InputManager;
+        inline static std::unique_ptr<ThreadPool> m_ThreadPool;
     };
 }
