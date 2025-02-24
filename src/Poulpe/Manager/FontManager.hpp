14f7b699a9b666a06af5f076d26affb8927dadd4
galliume <gcconantc@gmail.com>
2025-02-24 08:22:49 +0100

    atlas

--- src/Poulpe/Manager/FontManager.hpp
+++ src/Poulpe/Manager/FontManager.hpp
@@ -14,14 +14,37 @@ namespace Poulpe
 {
   class Renderer;
 
+  struct FontAtlas {
+    unsigned int mem_size{ 0 };
+    unsigned int width{0};
+    unsigned int height{0};
+    unsigned int count{0};
+    std::array<std::array<unsigned char*, 16>, 16> buffer;
+
+    std::array<unsigned char*, 256> flatten() const
+    {
+      std::array<unsigned char*, 256> flattened;
+      size_t index = 0;
+
+      auto const row_width{ 16 };
+      auto const row_height{ 16 };
+
+      for (auto i{ 0 }; i < row_height; ++i) {
+        for (auto j{ 0 }; j < row_width; ++j) {
+          flattened[i * row_width + j] = buffer[i][j];
+        }
+      }
+      return flattened;
+    }
+  };
+
   class FontManager
   {
-
   public:
 
     struct character {
       unsigned int tex_id;
-      Texture texture;
+      float offset;
       glm::ivec2 size;
       glm::ivec2 bearing;
       long advance;
